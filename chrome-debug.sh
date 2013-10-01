c92810838054ae936783dc36cef00141a73ec97d
Willie Miller <willie.miller@audaxhealth.com>
2013-09-30 22:39:46 -0400

    attenpting to fix chrome-debug script

--- chrome-debug.sh
+++ chrome-debug.sh
@@ -1,1 +1,1 @@
- open -a Google\ Chrome --args --disable-web-security --allow-file-access-from-files backbone/index.html
+ open -a Google\ Chrome --args --user-data-dir="." --disable-web-security --allow-file-access-from-files
